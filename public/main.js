6392abe17cfb8cfa0d7816bec2e1dff13fc662a2
schego1ev <schegolev.s.n@gmail.com>
2026-02-25 15:38:23 +0300

    Add logic of load items

--- public/main.js
+++ public/main.js
@@ -8,7 +8,40 @@ async function loadItems() {
   let response = await fetch('/api/items');
   
   if (response.ok) {
-    let json = await response.json();
-    console.log(json);
+    const json = await response.json();
+    const shoppingList = document.querySelector('.shopping-list');
+    
+    shoppingList.innerHTML = '';
+
+    json.forEach(element => {
+      const listItem = document.createElement('li');
+      
+      listItem.textContent = element;
+      shoppingList.append(listItem);
+    });
   }
 }
+
+loadItems();
+
+const input = document.querySelector('#input');
+const submitButton = document.querySelector('.submit');
+
+submitButton.addEventListener('click', async () => {
+  const value = input.value;
+
+if (input.value.trim().length !== 0) {
+  await fetch('/api/items', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({ text: value })
+  });
+  
+  loadItems();
+  input.value = '';
+} else {
+  return;
+}
+});
